b0f1f59394698b40c0fb1c618558cc536a9bdf2a
Griffin Riley <rileyg@oregonstate.edu>
2024-04-03 12:37:45 -0700

    solved part 2

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import math
+
 
 def solvepart1():
     #read data into 2 dictionaries
@@ -50,7 +52,8 @@ def pressButton():
         if incomingSignal == 0: totalLow += 1
         else: totalHigh += 1
 
-        if moduleName == "rx": continue
+        if moduleName == "rx":
+            continue
 
         if moduleType == "%":
             if incomingSignal == 0:
@@ -76,6 +79,90 @@ def pressButton():
 
     return totalLow, totalHigh
 
+def solvepart2():
+    #read data into 2 dictionaries
+    data = fileRead("input.txt")
+    global modules
+    modules = {"button": ("*", ("broadcaster",)), "rx": ("*", ())}
+    global modulesState
+    modulesState = {"button": {}}
+    for row in data:
+        splitRow = row.split(" -> ")
+        nameType = splitRow[0]
+        destinationsStr = splitRow[1]
+        if ("%" in nameType) or ("&" in nameType):
+            name = nameType[1:]
+            moduleType = nameType[0]
+        else:
+            name = nameType
+            moduleType = "*"
+        destinations = tuple(destinationsStr.strip().split(", "))
+        modules[name] = (moduleType, destinations)
+        modulesState[name] = {}
+    for module, data in modules.items():
+        for outModule in data[1]:
+            if outModule != "rx":
+                modulesState[outModule][module] = 0
+
+    # #press button until a cycle has been found for xc, th, bp, and pd
+    numpresses = 0
+    cycles = {}
+    while True:
+        newCycles = pressButtonCycleCheck()
+        numpresses += 1
+        
+        for k,v in newCycles.items():
+            if v:
+                cycles[k] = numpresses
+
+        if len(cycles) >= 4:
+            break
+
+    print(cycles)
+    print(math.lcm(*list(cycles.values())))
+
+#propogate signal through modules, breadth-first, updating global modules state as it goes, looking for cycles for xc, th, bp, and pd
+def pressButtonCycleCheck():
+    cycles = {}
+    queue = [("broadcaster",0,"button")]
+    while len(queue) > 0:
+        currentModule = queue.pop(0)
+        moduleName = currentModule[0]
+        incomingSignal = currentModule[1]
+        prevModule = currentModule[2]
+        moduleType = modules[moduleName][0]
+        outModules = modules[moduleName][1]
+
+        if moduleName == "rx":
+            continue
+        for module in ("xc","th","bp","pd"):
+            if moduleName == module and incomingSignal == 0:
+                cycles[module] = True
+
+        if moduleType == "%":
+            if incomingSignal == 0:
+                state = modulesState[moduleName].get(0, 0)
+                state = 1-state
+                modulesState[moduleName][0] = state
+                for module in outModules:
+                    queue.append((module, state, moduleName))
+        elif moduleType == "&":
+            modulesState[moduleName][prevModule] = incomingSignal
+            allHigh = True
+            for value in modulesState[moduleName].values():
+                if value == 0:
+                    allHigh = False
+                    break
+            if allHigh: outState = 0
+            else: outState = 1
+            for module in outModules:
+                    queue.append((module, outState, moduleName))
+        else:
+            for module in outModules:
+                queue.append((module, incomingSignal, moduleName))
+
+    return cycles
+
 def fileRead(name):
     data = []
     f = open(name, "r")
@@ -83,4 +170,4 @@ def fileRead(name):
         data.append(line);
     return data
 
-solvepart1()
+solvepart2()
